19963508cc88d82c5637179d93a036b8bf46ba49
Daniel Irvine <daniel@danielirvine.com>
2015-03-02 21:58:41 +0000

    Implements strike

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -3,10 +3,15 @@
             [bowling.core :refer :all]))
 
 (defn new-score [rolls]
-  (apply + (map-indexed (fn [i v] (if (and (< i (- (count rolls) 2))
-                                           (= 10 (+ (nth rolls (inc i)) v)))
-                                    (+ v (nth rolls (+ i 2)))
-                                    v)) rolls)))
+  (apply + (map-indexed (fn [i v]
+                          (cond
+                            (and (< i (- (count rolls) 2))
+                                 (= 10 v))
+                            (+ v (nth rolls (+ i 2)) (nth rolls (+ i 1)))
+                            (and (< i (- (count rolls) 2))
+                                 (= 10 (+ (nth rolls (inc i)) v)))
+                            (+ v (nth rolls (+ i 2)))
+                            :else v)) rolls)))
 
 (defn- score [rolls]
   (new-score (vec rolls)))
@@ -17,4 +22,8 @@
   (it "scores a game with all ones"
     (should= 20 (new-score (repeat 20 1))))
   (it "scores a game with a spare"
-    (should= 16 (new-score (concat '(5 5 3) (repeat 17 0))))))
+    (should= 16 (new-score (concat '(5 5 3) (repeat 17 0)))))
+  (it "scores a game with a strike"
+    (should= 20 (new-score (concat '(10 5) (repeat 18 0)))))
+  (it "scores a perfect game"
+    (should= 300 (new-score (repeat 12 10)))))
