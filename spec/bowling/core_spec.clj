85616664c475485fb1b0a2e0a3d5fc584a04763e
Daniel Irvine <daniel@danielirvine.com>
2015-03-02 21:16:36 +0000

    Reworks score to work on non-pairs

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -3,18 +3,18 @@
             [bowling.core :refer :all]))
 
 (defn new-score [rolls]
-  (apply + (map-indexed #(if (= %2 '(5 5))
-                  (+ 10 (first (get (vec rolls) (inc %1))))
-                  (apply + %2))
-               rolls)))
+  (apply + (map-indexed (fn [i v] (if (and (< i (- (count rolls) 2))
+                                           (= 10 (+ (nth rolls (inc i)) v)))
+                                    (+ v (nth rolls (+ i 2)))
+                                    v)) rolls)))
 
-(defn score [rolls]
-  (new-score (partition-all 2 rolls)))
+(defn- score [rolls]
+  (new-score (vec rolls)))
 
-(describe "score"
+(describe "new-score"
   (it "scores a zero game"
-    (should= 0 (score (repeat 20 0))))
+    (should= 0 (new-score (repeat 20 0))))
   (it "scores a game with all ones"
-    (should= 20 (score (repeat 20 1))))
+    (should= 20 (new-score (repeat 20 1))))
   (it "scores a game with a spare"
-    (should= 16 (score (concat '(5 5 3) (repeat 17 0))))))
+    (should= 16 (new-score (concat '(5 5 3) (repeat 17 0))))))
