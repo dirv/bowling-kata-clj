f2ef10c7db0bd9f2c9bf13b36479c45f8539aacf
Daniel Irvine <daniel@danielirvine.com>
2015-03-19 14:16:07 +0000

    Using functions

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,1 +1,22 @@
 (ns bowling.core)
+
+(defn- full-scores [rolls]
+  (take 21 (concat rolls (repeat 0))))
+
+(defn- to-frames [remaining [frame & frames]]
+  (if (empty? remaining)
+    (conj frames frame)
+  (cond (= '(10) frame)
+        (to-frames remaining (conj frames (conj frame (first remaining) (second remaining)) '()))
+        (= 10 (apply + frame))
+        (to-frames remaining (conj frames (conj frame (first remaining)) '()))
+        (= 2 (count frame))
+        (to-frames remaining (conj frames frame '()))
+        :else
+        (to-frames (rest remaining) (conj frames (conj frame (first remaining)))))))
+
+(defn- to-frames2 [rolls]
+  (take-last 10 (to-frames rolls '())))
+
+(defn score [rolls]
+  (apply + (flatten (to-frames2 (full-scores rolls)))))
