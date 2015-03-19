ce5168a2997bf9721afc6dd7dd7746383568e7a8
Daniel Irvine <daniel@danielirvine.com>
2015-03-19 14:22:36 +0000

    Splits implementation from tests

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -2,29 +2,6 @@
   (:require [speclj.core :refer :all]
             [bowling.core :refer :all]))
 
-(defn- full-scores [rolls]
-  (take 21 (concat rolls (repeat 0))))
-
-; potential variations to show--total
-; using take a loop/recur
-(defn to-frames [rolls]
-  (loop [frame '()
-         all-frames '()
-         remaining rolls]
-    (cond (= 10 (count all-frames))
-          all-frames
-          (= '(10) frame)
-          (recur '() (conj all-frames (conj frame (first remaining) (second remaining))) remaining)
-          (= 10 (apply + frame))
-          (recur '() (conj all-frames (conj frame (first remaining))) remaining)
-          (= 2 (count frame))
-          (recur '() (conj all-frames frame) remaining)
-          :else
-          (recur (conj frame (first remaining)) all-frames (rest remaining)))))
-
-(defn- score [rolls]
-  (apply + (flatten (to-frames (full-scores rolls)))))
-
 (describe "new-score"
   (it "scores a zero game"
     (should= 0 (score (repeat 20 0))))
--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,1 +1,22 @@
 (ns bowling.core)
+
+(defn- full-scores [rolls]
+  (take 21 (concat rolls (repeat 0))))
+
+(defn to-frames [rolls]
+  (loop [frame '()
+         all-frames '()
+         remaining rolls]
+    (cond (= 10 (count all-frames))
+          all-frames
+          (= '(10) frame)
+          (recur '() (conj all-frames (conj frame (first remaining) (second remaining))) remaining)
+          (= 10 (apply + frame))
+          (recur '() (conj all-frames (conj frame (first remaining))) remaining)
+          (= 2 (count frame))
+          (recur '() (conj all-frames frame) remaining)
+          :else
+          (recur (conj frame (first remaining)) all-frames (rest remaining)))))
+
+(defn score [rolls]
+  (apply + (flatten (to-frames (full-scores rolls))))) 
