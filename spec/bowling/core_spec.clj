7e52e7377bd88e2ad1715fe14c22302737a2897d
Daniel Irvine <daniel@danielirvine.com>
2015-03-19 15:56:41 +0000

    Fixes strike test and splits out files

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -2,20 +2,6 @@
   (:require [speclj.core :refer :all]
             [bowling.core :refer :all]))
 
-(defn new-score [rolls]
-  (apply + (map-indexed (fn [i v]
-                          (cond
-                            (and (< i (- (count rolls) 2))
-                                 (= 10 v))
-                            (+ v (nth rolls (+ i 2)) (nth rolls (+ i 1)))
-                            (and (< i (- (count rolls) 2))
-                                 (= 10 (+ (nth rolls (inc i)) v)))
-                            (+ v (nth rolls (+ i 2)))
-                            :else v)) rolls)))
-
-(defn- score [rolls]
-  (new-score (vec rolls)))
-
 (describe "new-score"
   (it "scores a zero game"
     (should= 0 (new-score (repeat 20 0))))
@@ -24,6 +10,6 @@
   (it "scores a game with a spare"
     (should= 16 (new-score (concat '(5 5 3) (repeat 17 0)))))
   (it "scores a game with a strike"
-    (should= 20 (new-score (concat '(10 5) (repeat 18 0)))))
+    (should= 30 (new-score (concat '(10 5 5) (repeat 17 0)))))
   (it "scores a perfect game"
     (should= 300 (new-score (repeat 12 10)))))
--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,1 +1,15 @@
 (ns bowling.core)
+
+(defn new-score [rolls]
+  (apply + (map-indexed (fn [i v]
+                          (cond
+                            (and (< i (- (count rolls) 2))
+                                 (= 10 v))
+                            (+ v (nth rolls (+ i 2)) (nth rolls (+ i 1)))
+                            (and (< i (- (count rolls) 2))
+                                 (= 10 (+ (nth rolls (inc i)) v)))
+                            (+ v (nth rolls (+ i 2)))
+                            :else v)) rolls)))
+
+(defn- score [rolls]
+  (new-score (vec rolls)))
