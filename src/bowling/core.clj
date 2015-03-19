5d43da4e5572cb7cdd82cbf00ab126a9b8008370
Daniel Irvine <daniel@danielirvine.com>
2015-03-19 17:40:10 +0000

    Refactors to get rid of first frame concept

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -3,18 +3,19 @@
 (defn- strike? [remaining]
   (= 10 (first remaining)))
 
-(defn- spare?  [remaining]
+(defn- spare? [remaining]
   (= 10 (+ (first remaining) (second remaining))))
 
 (defn- to-frames [remaining frames]
-  (if (empty? remaining)
-    frames  
-    (cond (strike? remaining) 
-        (to-frames (drop 1 remaining) (conj frames (take 3 remaining)))
-        (spare? remaining) 
-        (to-frames (drop 2 remaining) (conj frames (take 3 remaining)))
-        :else
-        (to-frames (drop 2 remaining) (conj frames (take 2 remaining))))))
+  (cond 
+    (empty? remaining)
+    frames
+    (strike? remaining) 
+    (to-frames (drop 1 remaining) (concat frames (list (take 3 remaining))))
+    (spare? remaining) 
+    (to-frames (drop 2 remaining) (concat frames (list (take 3 remaining))))
+    :else
+    (to-frames (drop 2 remaining) (concat frames (list (take 2 remaining))))))
 
 (defn score [rolls]
-  (apply + (flatten (take-last 10 (to-frames rolls '())))))
+  (apply + (flatten (take 10 (to-frames rolls '())))))
