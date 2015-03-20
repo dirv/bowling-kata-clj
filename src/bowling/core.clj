6bda3bdd5de4d45181c9935c0fc8267da48ec6d7
Daniel Irvine <daniel@danielirvine.com>
2015-03-20 09:18:17 +0000

    Uses iterate again

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -6,16 +6,15 @@
 (defn- spare? [remaining]
   (= 10 (+ (first remaining) (second remaining))))
 
-(defn- to-frames [remaining frames]
+(defn- to-frame [[remaining frames]]
   (cond 
-    (empty? remaining)
-    frames
     (strike? remaining) 
-    (to-frames (drop 1 remaining) (concat frames (list (take 3 remaining))))
+    [(drop 1 remaining) (conj frames (take 3 remaining))]
     (spare? remaining) 
-    (to-frames (drop 2 remaining) (concat frames (list (take 3 remaining))))
+    [(drop 2 remaining) (conj frames (take 3 remaining))]
     :else
-    (to-frames (drop 2 remaining) (concat frames (list (take 2 remaining))))))
+    [(drop 2 remaining) (conj frames (take 2 remaining))])
+    )
 
 (defn score [rolls]
-  (apply + (flatten (take 10 (to-frames rolls '())))))
+  (apply + (flatten (second (nth (iterate to-frame [rolls '()]) 10)))))
